61a5816bd36f4da32b15bc567c8c6f681ebd05f2
Xiaoyi Liu <lxy878@gmail.com>
2020-08-30 16:59:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function superbowlWin(array){
   win = array.find(obj => obj.result === 'W' );
-  if (win) return win.year
+  if (win) return win.year;
 }
 const record = [
   { year: "2018", result: "N/A"},
